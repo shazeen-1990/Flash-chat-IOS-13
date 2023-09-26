f95032b45aa121d8d83978a2b7f05e096555b0af
Shazeen Thowfeek <114829290+shazeen-1990@users.noreply.github.com>
2023-09-26 09:37:29 +0530

    Add files via upload

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Shazeen Thowfeek on 25/09/2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message{
+    let sender : String
+    let body: String
+    
+    
+}
